029eabf4da99a4a6c1a45cadaaab799e750fe4e5
Aitor <Aitor@DESKTOP-QBVFBAF.mshome.net>
2024-04-24 11:02:11 +0200

    Projecto de quiniela

--- JavaApplication8/src/javaapplication8/Principal.java
+++ JavaApplication8/src/javaapplication8/Principal.java
@@ -1,3 +1,4 @@
+package javaapplication8;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
